3649d13ada3a59fe965fa9c61d231acc953cb693
Jose Ramirez <jrramirez1994@gmail.com>
2017-03-15 12:28:38 -0700

    updated database

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'goawayjo_sunnies');
+define('DB_NAME', 'sunnies2');
 
 /** MySQL database username */
-define('DB_USER', 'goawayjo_jose');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'haha626');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
